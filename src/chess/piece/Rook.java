205f442bd1a1a9e6bb543e73c474a0a85c5e989c
loulemaire <lou.lemaire@insa-lyon.fr>
2022-03-15 16:19:50 +0100

    rook

--- src/chess/piece/Rook.java
+++ src/chess/piece/Rook.java
@@ -8,45 +8,23 @@ import chess.player.Team;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Rook extends Piece{
+public class Rook extends SlidingPiece {
 
-    private boolean hasMoved; //Pour le roque
+    private static final Position[] DIRECTIONS = {
+            // Lignes
+            new Position(0, 1),
+            new Position(0, -1),
+            new Position(1, 0),
+            new Position(-1, 0)
+    };
 
-    public Rook (Team team, Position position) {
+    public Rook(Team team, Position position) {
         super(team, position);
-        hasMoved = false ;
+
     }
 
-    public Set<Move> computePossibleMoves(Board board) {
-        Set<Move> moves = new HashSet<>();
-        for(int i = -1; i <= 1; i+=2) {
-            Position nextPosition = this.position.add(0, i);
-            while (nextPosition.isInBoard()) {
-                if( board.getPiece(nextPosition) == null) {
-                    moves.add(new Move(this.position, nextPosition, this));
-                    nextPosition = nextPosition.add(0, i);
-                }
-                if (board.getPiece(nextPosition) != null && board.getPiece(nextPosition).getTeam() != this.team) {
-                    moves.add(new Move(this.position, nextPosition, this, board.getPiece(this.position)));
-                    break;
-                }
-            }
-        }
-        for(int i = -1; i <= 1; i+=2) {
-            Position nextPosition = this.position.add(i, 0);
-            while (nextPosition.isInBoard()) {
-                if( board.getPiece(nextPosition) == null) {
-                    moves.add(new Move(this.position, nextPosition, this));
-                    nextPosition = nextPosition.add(i, 0);
-                }
-                if (board.getPiece(nextPosition) != null && board.getPiece(nextPosition).getTeam() != this.team) {
-                    moves.add(new Move(this.position, nextPosition, this, board.getPiece(this.position)));
-                    break;
-                }
-            }
-        }
-        return moves;
+    @Override
+    protected Position[] getSlidingDirections() {
+        return new Position[0];
     }
-
-
 }

049579635f2d190589a5115898901d468131f6d1
Kowsigan <hiruma1002@gmail.com>
2022-03-15 16:25:06 +0100

    chevalierV2

--- src/chess/piece/Knight.java
+++ src/chess/piece/Knight.java
@@ -10,8 +10,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Knight extends Piece {
-    private int x;
-    private int y;
+    
+    private static final Position[] DIRECTIONS = {
+            new Position(1, 2),
+            new Position(1, -2),
+            new Position(2, 1),
+            new Position(2, -1),
+            new Position(-1, 2),
+            new Position(-1, -2),
+            new Position(-2, 1),
+            new Position(-2, -1)
+    };
+    
 
     public Knight(Team team, Position position) {
         super(team, position);
@@ -20,23 +30,18 @@ public class Knight extends Piece {
 
     public Set<Move> computePossibleMoves(Board board) {
         Set<Move> moves = new HashSet<>();
-
-        ArrayList<Position> casePossible = new ArrayList<Position>(); //maybe changer en tableau statique
-        casePossible.add(this.position.add(1, 2));
-        casePossible.add(this.position.add(1, -2));
-        casePossible.add(this.position.add(2, 1));
-        casePossible.add(this.position.add(2, -1));
-        casePossible.add(this.position.add(-2, 1));
-        casePossible.add(this.position.add(-2, -1));
-        casePossible.add(this.position.add(-1, 2));
-        casePossible.add(this.position.add(-1, -2));
-
-        for (Position test : casePossible) {
-            if (test.isInBoard() && (board.getPiece(test) == null || (board.getPiece(test) != null && board.getPiece(test).getTeam() != this.team)) ) {
-                moves.add(new Move(this.position, test, this,board.getPiece(test))); //renvoie null si pas de pion
+        
+        
+        for (Position direction : DIRECTIONS) {
+        
+           Position test = direction.add(this.position);
+            if (test.isInBoard() && (board.getPiece(test) == null || (board.getPiece(test) != null && board.getPiece(test).getTeam() != this.team))) {
+                moves.add(new Move(this.position, test, this, board.getPiece(test))); //renvoie null si pas de pion
             }
 
         }
         return moves;
     }
+
+    
 }

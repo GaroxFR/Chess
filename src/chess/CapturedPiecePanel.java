d9833ef4dfac46ebcea5f7329011978a3010be64
Kowsigan <hiruma1002@gmail.com>
2022-04-04 21:16:20 +0200

    panel captured V2

--- src/chess/CapturedPiecePanel.java
+++ src/chess/CapturedPiecePanel.java
@@ -19,44 +19,62 @@ public class CapturedPiecePanel extends JPanel {
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        int iPawn = 8;
+        int jPawn = 8;
+        int iKnight = 8;
+        int jKnight = 56;
+        int iBishop = 8;
+        int jBishop = 104;
+        int iRook = 8;
+        int jRook = 152;
+        int iQueen = 8;
+        int jQueen = 200;
         for (Piece captured : this.board.getCapturedPiece() ) {
+
+
             if (captured.getTeam() != this.team) {
                 continue;
             }
 
             if(captured instanceof Pawn){
-                int i = 32;
-                int j = 32;
-                g.drawImage(captured.getImage(), i, j, 16, 16, null);
-                i = i + 32;
-                if(i == 224){ //4 pions max par ligne
-                    i=0;
-                    j=j+64;
+                g.drawImage(captured.getImage(), iPawn, jPawn, 16, 16, null);
+                iPawn = iPawn + 16;
+                if(iPawn > 56){ //4 pions max par ligne
+                    iPawn=8;
+                    jPawn=jPawn+16;
                 }
             }
             if(captured instanceof Knight){
-                int i= 32;
-                int j=160;
-                g.drawImage(captured.getImage(), i, j, 16, 16, null);
-                i = i + 64;
+                g.drawImage(captured.getImage(), iKnight, jKnight, 16, 16, null);
+                iKnight = iKnight + 16;
+                if(iKnight > 56){ //4 pions max par ligne
+                    iKnight=8;
+                    jKnight=jKnight+16;
+                }
             }
             if(captured instanceof Bishop){
-                int i= 32;
-                int j=224;
-                g.drawImage(captured.getImage(), i, j, 16, 16, null);
-                i = i + 64;
+                g.drawImage(captured.getImage(), iBishop, jBishop, 16, 16, null);
+                iBishop = iBishop + 16;
+                if(iBishop > 56){ //4 pions max par ligne au cas ou pour la promotion
+                    iBishop=8;
+                    jBishop=jBishop+16;
+                }
             }
             if(captured instanceof Rook){
-                int i= 32;
-                int j=288;
-                g.drawImage(captured.getImage(), i, j, 16, 16, null);
-                i = i + 64;
+                g.drawImage(captured.getImage(), iRook, jRook, 16, 16, null);
+                iRook = iRook + 16;
+                if(iRook > 56){ //4 pions max par ligne
+                    iRook=8;
+                    jBishop=jBishop+16;
+                }
             }
             if(captured instanceof Queen){
-                int i= 32;
-                int j=352;
-                g.drawImage(captured.getImage(), i, j, 16, 16, null);
-                i = i + 64;
+                g.drawImage(captured.getImage(), iQueen, jQueen, 16, 16, null);
+                iQueen = iQueen + 16;
+                if(iQueen > 56){ //4 pions max par ligne
+                    iQueen=8;
+                    jQueen=jQueen+16;
+                }
             }
         }
     }
